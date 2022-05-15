56aa265170689a9fb464bdc1b48029ef8bcc2bd1
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-15 11:48:56 -0500

    [calendar] se configuro el idioma al español, y se mejoro el dieño del calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,11 +1,49 @@
 import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 import { Navbar } from "../ui/Navbar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import "moment/locale/es";
+import { messages } from "../../helpers/calendar-messages-es";
+moment.locale("es");
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "Cumpleaños del jefe",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+  },
+];
 
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#367CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+
+    return {
+      style,
+    };
+  };
+
   return (
-    <div>
+    <div className="calendar-screen">
       <Navbar />
-      <h1>CalendarScreen </h1>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        // style={{ height: 1000 }}
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
+      />
     </div>
   );
 };
