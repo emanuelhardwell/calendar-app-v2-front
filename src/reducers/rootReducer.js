f8801cc9d5639aec74cbb13c54ecfd558dbf4bad
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-16 10:03:17 -0500

    [REDUX] se configuro redux en nuestra app, creamos el uiReducer y los types relacionados al Modal

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import { uiReducer } from "./uiReducer";
+
+export const rootReducer = combineReducers({
+  ui: uiReducer,
+});
---
+++ src/types/types.js
@@ -0,0 +1,4 @@
+export const types = {
+  uiOpenModal: "[ui] Open modal",
+  uiCloseModal: "[ui] Close modal",
+};
